36ee02e5bdf2aa2351417884c1fac100fbf696f3
romesharon <33751805+romesharon@users.noreply.github.com>
2018-04-11 21:54:07 +0300

    HW 1 Almost Done
    To-Do:
    1. add comments
    2. maybe some more checks

---
+++ ImageService/ImageService/Modal/ImageServiceModal.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImageService.Modal
+{
+    public class ImageServiceModal : IImageServiceModal
+    {
+        #region Members
+        private string m_OutputFolder;            // The Output Folder
+        private int m_thumbnailSize;              // The Size Of The Thumbnail 
+        //private static Regex r = new Regex(":");
+
+        public ImageServiceModal(string outputFolder, int thumbnailSize)
+        {
+            this.m_OutputFolder = outputFolder;
+            this.m_thumbnailSize = thumbnailSize;
+        }
+
+        public string AddFile(string path, out bool result)
+        {
+            string imageName = Path.GetFileName(path);
+            //  check if the file is exist
+            if (!File.Exists(path))
+            {
+                result = false;
+                return "Error: the file is not exist";
+            }
+
+            //  get the date of the image
+            DateTime date = File.GetCreationTime(path);
+            string year = date.Year.ToString();
+            string month = date.Month.ToString();
+
+            //  create directory's
+            Directory.CreateDirectory(Path.Combine(this.m_OutputFolder,year,month));
+            Directory.CreateDirectory(Path.Combine(this.m_OutputFolder, "thumbnails", year, month));
+
+            int counter = 0;
+
+            //  check if thumbnail file is 
+            string destThumbPath = string.Format("{0}\\{1}\\{2}\\{3}\\{4}", m_OutputFolder, "thumbnails", year, month, imageName);
+            if (!File.Exists(destThumbPath))
+            {
+                //  get the thumbnail of the image
+                using (Image image = Image.FromFile(path))
+                using (Image thumbnail = image.GetThumbnailImage(this.m_thumbnailSize, this.m_thumbnailSize, () => false, IntPtr.Zero))
+                {
+                    thumbnail.Save(destThumbPath);
+                }
+            }
+            else
+            {
+                counter = 1;
+                destThumbPath = string.Format("{0}\\{1}\\{2}\\{3}\\({4}){5}", m_OutputFolder, "thumbnails", year, month, counter, imageName);
+                while (File.Exists(destThumbPath))
+                {
+                    counter++;
+                    destThumbPath = string.Format("{0}\\{1}\\{2}\\{3}\\({4}){5}", m_OutputFolder, "thumbnails", year, month, counter, imageName);
+                }
+                using (Image image = Image.FromFile(path))
+                using (Image thumbnail = image.GetThumbnailImage(this.m_thumbnailSize, this.m_thumbnailSize, () => false, IntPtr.Zero))
+                {
+                    thumbnail.Save(destThumbPath);
+                }
+            }
+
+            if (counter > 0)
+            {
+                imageName = string.Format("({0}){1}", counter, imageName);
+            }
+            string destPath = string.Format("{0}\\{1}\\{2}\\{3}", m_OutputFolder, year, month, imageName);
+            if (!File.Exists(destPath))
+            {
+                File.Move(path, destPath);
+            }
+            result = true;
+            return destPath;
+        }
+
+        #endregion
+
+    }
+}
---
+++ ImageService/ImageService/Server/ImageServer.cs
@@ -0,0 +1,70 @@
+﻿using ImageService.Controller;
+using ImageService.Controller.Handlers;
+using ImageService.Infrastructure.Enums;
+using ImageService.Logging;
+using ImageService.Modal;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Server
+{
+    public class ImageServer
+    {
+        #region Members
+        private IImageController m_controller;
+        private ILoggingService m_logging;
+        #endregion
+
+        #region Properties
+        // The event that notifies about a new Command being recieved
+        public event EventHandler<CommandRecievedEventArgs> CommandRecieved;         
+        #endregion
+
+        public ImageServer(IImageController controller, ILoggingService logging)
+        {
+            this.m_controller = controller;
+            this.m_logging = logging;
+
+
+            string str = ConfigurationManager.AppSettings.Get("Handler");
+            string[] directories = str.Split(';');
+
+            foreach (string dir in directories)
+            {
+                this.m_logging.Log(string.Format("directory to listen {0}", dir), Logging.Modal.MessageTypeEnum.INFO);
+                CreateHandler(dir);
+            }
+        }
+
+        public void CreateHandler(string directory)
+        {
+            //  creating the handler
+            IDirectoryHandler directoryHandler = new DirectoyHandler(this.m_controller, this.m_logging);
+            //  adding the handler to the 'event'
+            this.CommandRecieved += directoryHandler.OnCommandRecieved;
+            //  set the closing handler function
+            directoryHandler.DirectoryClose += CloseServer;
+            //  start the handler for this directory
+            directoryHandler.StartHandleDirectory(directory);
+        }
+
+        public void SendCommand(CommandRecievedEventArgs e)
+        {
+            //  sending the command to all handlers
+            this.CommandRecieved?.Invoke(this, e);
+        }
+
+        public void CloseServer(object source, DirectoryCloseEventArgs e)
+        {
+            if (source is IDirectoryHandler directoryHandler)
+            {
+                this.CommandRecieved -= directoryHandler.OnCommandRecieved;
+                directoryHandler.DirectoryClose -= CloseServer; //maybe a mistake..
+            }
+        }
+    }
+}
